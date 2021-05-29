32ac654d2c08d46d69caf8776782012bd58ada38
janlankas <lankascb@gmail.com>
2021-05-29 16:30:50 +0200

    sliders

--- app.js
+++ app.js
@@ -2,9 +2,13 @@
 // Global selection
 const colorDivs = document.querySelectorAll('.color');
 const generateBtn = document.querySelector('.generate');
-const sliders = document.querySelectorAll('input [type="range"]');
+const sliders = document.querySelectorAll('input[type="range"]');
 const currentHex = document.querySelectorAll('.color h2');
 let initialColors;
+
+
+// Add our event listeners 
+sli
 //Functions
 // Color generator
 function generateHex() {
@@ -22,7 +26,42 @@ function randomColors() {
         // Add the color to the bg
         div.style.backgroundColor = randomColor;
         hexText.innerText = randomColor;
+        //Check for contrast 
+        checkTextContrast(randomColor, hexText);
+        //Initial color sliders
+        const color = chroma(randomColor);
+        const sliders = div.querySelectorAll(".sliders input");
+        const hue = sliders[0];
+        const brightness = sliders[1];
+        const saturation = sliders[2];
+
+
+        colorizeSliders(color, hue, brightness, saturation);
     });
+
+}
+
+function checkTextContrast(color, text) {
+    const luminance = chroma(color).luminance();
+    if (luminance > 0.5) {
+        text.style.color = "black";
+    } else {
+        text.style.color = "white";
+    }
+}
+
+function colorizeSliders(color, hue, brightness, saturation) {
+    //Scale Sat
+    const noSat = color.set("hsl.s", 0);
+    const fullSat = color.set("hsl.s", 1);
+    const scaleSat = chroma.scale([noSat, color, fullSat]);
+    //Scale Brightness
+    const midBright = color.set("hsl.l", 0.5);
+    const scaleBrightness = chroma.scale(['black', midBright, 'white']);
+    // Input update colorizeSliders
+    saturation.style.backgroundImage = `linear-gradient(to right,${scaleSat(0)},${scaleSat(1)})`;
+    brightness.style.backgroundImage = `linear-gradient(to right,${scaleBrightness(0)}, ${scaleBrightness(0.5)},${scaleBrightness(1)})`;
+    hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75), rgb(204,204,75), rgb(75,204,75), rgb(75,204,204), rgb(75,75,204), rgb(204,75,204), rgb(204,75,75))`;
 
 }
 
